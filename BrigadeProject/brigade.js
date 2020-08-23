9b67840a9bc67a4580773cf10cf8b10f648ccd2f
rahul <rahuldhus766@gmail.com>
2020-08-24 00:12:35 +0530

    add commit for bridage testing

--- BrigadeProject/brigade.js
+++ BrigadeProject/brigade.js
@@ -1,5 +1,5 @@
 const { events } = require('brigadier');
 
 events.on('exec', () => {
-  console.log('Hello from brig! Welcome to Successive group.. ok testing started..!!!');
+  console.log('Hello from brig! Welcome to Successive group.. ok testing done with output..!!!');
 });
